9a93b4e1994fa8fc44e342664d77c2d59e2be241
Victor Inojosa <vijoin@gmail.com>
2020-03-27 12:18:39 -0300

    Add core app and initial Templates, including bootstrap

--- personal_expenses_control/urls.py
+++ personal_expenses_control/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('products/', include('products.urls')),
+    path('expenses/', include('expenses.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
